eda671877382c6107e9260d0a40898050b3f0df9
Kirstin Hartman and Nathan Mayer <nathan@>
2017-06-19 19:05:28 -0700

    refactor if statement to combine first two behaviors

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 //back-end
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
-} else {
+  // } else if (year % 4 === 0) {
+  //   return true;
+  } else {
   return false;
-}
+  }
 };
 
 
